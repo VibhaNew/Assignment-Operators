7a1e3c3deb6549e27f3d237589e57a6cc06ec440
MyPyCo <vibhavari.kulkarni@gmail.com>
2024-09-02 23:41:27 +0530

    Add files via upload

---
+++ Assignment Operators.py
@@ -0,0 +1,12 @@
+# Assignment Operators
+# += adds the value on the right to the value of the variable on the left.This assigns new value to the variable.
+# -=
+# *=
+# /=
+
+Marks = 23
+Marks+= 3   # 26
+Marks-= 2   # 24
+Marks*= 2   # 24*2=48
+
+print(Marks)
